238bff3f78e9e812263abffc8ac909fc01b06bdd
InoriReei <marlborofornemo@gmail.com>
2017-04-10 19:48:53 -0500

    Add files via upload
    added a search bar, not really working though

--- guestheader.php
+++ guestheader.php
@@ -41,5 +41,20 @@
 					</ul>
 					</div>
 				</div>
+				
+				<div class="container">
+					<div class="col-md-3">
+					  <form class="navbar-form" role="search">
+						<div class="input-group add-on">
+						  <input class="form-control" placeholder="Search" name="srch-term" id="srch-term" type="text">
+						  <div class="input-group-btn">
+							<button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+						  </div>
+						</div>
+					</div>
+				</div>
+				
+				
+				
 			</div>
 		</div>
