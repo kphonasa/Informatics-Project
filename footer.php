c818c120f4e767f2e06b9d584dd7887a8fff3435
InoriReei <marlborofornemo@gmail.com>
2017-04-19 15:01:06 -0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,5 +1,23 @@
 </div>
 </body>
 <footer>
+  </div>
+  <div class='row'>
+    <div class='col-xs-12'>
+
+		  <p>
+			  Contact us with: 319-###-####
+		  </p>
+		</div>
+	</div>
+
+
+
+
+</body>
+
+
+
+</html>
 </footer>
 </html>
