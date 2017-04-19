f96e1df52a82d3faea9404a0dcf16ffc5d7e0862
InoriReei <marlborofornemo@gmail.com>
2017-04-19 14:33:36 -0500

    Add files via upload

--- shopperheader.php
+++ shopperheader.php
@@ -2,7 +2,7 @@
 <head>
     <title><?php echo $title;?></title>
     
-    <neta name="viewport" content="width=device-widthe, initial-scale-1.0">
+    <meta name="viewport" content="width=device-widthe, initial-scale-1.0">
     
     <!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
@@ -14,6 +14,16 @@
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 	
 </head>
+
+	<style>
+		body  {
+			background-color: green;
+			color: black;
+			font-size: 20px;
+		}
+	</style>
+	
+
 
 
 <body>
@@ -29,18 +39,25 @@
         
 		<div class="row">
 			<div class="col-xs-12">
-				<div class="navbar navbar-inverse">
+				<div class="navbar navbar-inverse navbar-static-top">
 				<!--Menu-->
 					<div class="container fluid">
-					<ul class="nav nav-pills">
-						<li <?php if($menuActive==0){echo 'class="active"';}?>><a href="shopperhome.php">Home</a></li>
-						<li <?php if($menuActive==1){echo 'class="active"';}?>><a href="browseP.php">Browse Products</a></li>
-						<li <?php if($menuActive==2){echo 'class="active"';}?>><a href="browseC.php">Browse Categories</a></li>
-						<li <?php if($menuActive==3){echo 'class="active"';}?>><a href="orders.php">Orders</a></li>
-						<li <?php if($menuActive==4){echo 'class="active"';}?>><a href="profile.php">Profile</a></li>
-						<li <?php if($menuActive==5){echo 'class="active"';}?>><a href="cart.php">Shopping Cart</a></li>
-						<li <?php if($menuActive==6){echo 'class="active"';}?>><a href="shopperlogout.php">Logout</a></li>
-					</ul>
+						<ul class="nav nav-pills">
+							<li <?php if($menuActive==0){echo 'class="active"';}?>><a href="shopperhome.php">Home</a></li>
+							<li <?php if($menuActive==1){echo 'class="active"';}?>><a href="browseP.php">Browse Products</a></li>
+							<li <?php if($menuActive==2){echo 'class="active"';}?>><a href="browseC.php">Browse Categories</a></li>
+							<li <?php if($menuActive==3){echo 'class="active"';}?>><a href="orders.php">Orders</a></li>
+							<li <?php if($menuActive==4){echo 'class="active"';}?>><a href="profile.php">Profile</a></li>
+							<li <?php if($menuActive==5){echo 'class="active"';}?>><a href="cart.php">Shopping Cart</a></li>
+							<li <?php if($menuActive==6){echo 'class="active"';}?>><a href="shopperlogout.php">Logout</a></li>
+							<form action="get.php" method="get" class="navbar-form navbar-right" role="search">
+								<div class="input-group add-on">
+								  <input class="form-control" placeholder="Search for our products" name="srch-term" id="srch-term" type="text">
+								  <div class="input-group-btn">
+									<button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>							  </div>
+								</div>
+							</form>	
+						</ul>
 					</div>
 				</div>
 			</div>
--- shopperhome.php
+++ shopperhome.php
@@ -8,6 +8,85 @@
 	$menuActive=0;
 	include_once("shopperheader.php");
 ?>
+
+    <div class='row'>
+            <div class='col-xs-12'>
+
+		<p>
+			Welcome to our store, how about try some random items?
+		</p>
+			</div>
+	</div>
+	
+<!-- Content table here -->
+    <div class='row'>
+            <div class='col-xs-12'>
+<table class='table table-hover'>
+    <!-- header for the table -->
+    <thead>
+        <th></th>
+		<th>Name</th>
+        <th>Description</th>
+        <th>Category</th>
+        <th>Price</th>
+        <th>QTY</th>
+		<th> </th>
+
+    </thead>
+	
+	<?php
+    /*
+     *List all Productes that are in the DB
+     *
+     */    
+    // connect to the DB
+    $db = connectDB($DBHost, $DBUser, $DBPasswd, $DBName);
+	
+    // set up a query to get infor on the cars from the DB
+	$query = 'SELECT * FROM PRODUCT order by RAND() LIMIT 2';
+    
+    // run the query
+    $result = queryDB($query, $db);
+    
+	
+    while($row = nextTuple($result)) {
+        echo "\n <tr>";
+		if ($row['IMAGE'])
+		{$imagelocation=$row['IMAGE'];
+		$altText="product" . $row['PNAME'];
+		echo "<td><img src='$imagelocation' width='150' alt=$altText'>";}
+		echo'</td>';
+        echo "<td>" .$row['PNAME'] . "</td>";
+        echo "<td>" .$row['DDESCRIPTION'] . "</td>";
+        echo "<td>" .$row['CATEGORY'] . "</td>";
+        echo "<td>" .$row['PRICE'] . "</td>";
+        echo "<td>" .$row['QTY'] . "</td>";
+		echo "<td>" .'<a href="' . 'ordersguest.php' . '">Add to Cart</a>' . "</td>";
+		
+		echo '<td>';
+        echo "</tr> \n";
+    }
+?>
+	
+	</table>	
+
+	
+	
+	
+	
+	
+	
+	</body>
+</html>
+
+
+
+
+
+
+
+
+
 <?php
 	include_once("footer.php");
 ?>
