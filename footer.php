6c759846747cea1e2076c87b04602573a8cb39a5
InoriReei <marlborofornemo@gmail.com>
2017-04-28 05:30:27 -0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,8 +1,11 @@
-	
-	<footer>
-	  <p>Designed by: GROUP 2</p>
-	  <p>Contact information: xxxxxxxxx</p>
-	</footer>
 
-	</body>
+<HR>
+<footer>
+	<p>Posted by: Group 2</p>
+	<p>Contact information: <a href="mailto:someone@example.com">someone@example.com</a>.</p>
+</footer>
+
+<p><strong>Note:</strong> The footer tag is not supported AT ALL</p>
+
+</body>
 </html>
