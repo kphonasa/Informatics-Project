43cadfe80b152c71edbc5941a02af124084abcce
InoriReei <marlborofornemo@gmail.com>
2017-04-30 17:25:20 -0500

    Update shopperhome.php

--- shopperhome.php
+++ shopperhome.php
@@ -1,89 +1,187 @@
 <!--shopper home-->
+
+<?php
+//kicks users out if they are not logged in
+	session_start();
+	if (!isset($_SESSION['email']))
+	{
+		header('Location: shopperlogin.php');
+		exit;
+	}
+?>
+
+
 <?php
 	include_once('config.php');
 	include_once('dbutils.php');
-
-	$title ="Home";
-	$h1 = "Home";
-	$menuActive=0;
-	include_once("shopperheader.php");
 ?>
-
-    <div class='row'>
-            <div class='col-xs-12'>
-
-		<p>
-			Welcome to our store, how about try some random items?
-		</p>
-			</div>
-	</div>
-	
-<!-- Content table here -->
-    <div class='row'>
-            <div class='col-xs-12'>
-<table class='table table-hover'>
-    <!-- header for the table -->
-    <thead>
-        <th></th>
-		<th>Name</th>
-        <th>Description</th>
-        <th>Category</th>
-        <th>Price</th>
-        <th>QTY</th>
-		<th> </th>
-
-    </thead>
-	
-	<?php
-    /*
-     *List all Productes that are in the DB
-     *
-     */    
-    // connect to the DB
-    $db = connectDB($DBHost, $DBUser, $DBPasswd, $DBName);
-	
-    // set up a query to get infor on the cars from the DB
-	$query = 'SELECT * FROM PRODUCT order by RAND() LIMIT 2';
+<html>
+	<head>
+	<meta name="viewport" content="width=device-widthe, initial-scale-1.0">
     
-    // run the query
-    $result = queryDB($query, $db);
-    
-	
-    while($row = nextTuple($result))
-		{
-			echo'<tr>';
-			echo'<td>';
-			
-			if ($row['IMAGE'])
-			{$imagelocation=$row['IMAGE'];
-			$altText="product" . $row['PNAME'];
-			echo "<img src='$imagelocation' width='150' alt=$altText'>";}  
-			echo'</td>';
-			echo "<td><a href='Description.php?ID=" . $row['ID'] . "'>" . $row['PNAME'] . "</a></td>";
-			echo '<td>' . $row['CATEGORY'] . '</td>';
-			echo '<td>'; echo"$"; echo $row['PRICE']; echo'</td>';
-			echo '<td>'; echo"Quantity"; echo"<form method='post' action='browsePguest.php?action=add&code='" . ['ID'] . "'><input type='text' name='quantity' size='2'/>"; echo '</td>';
-			echo '<td>'; echo"<button type ='submit' class='btn btn-default' name='Add'>Add to Cart</button></form>";echo'</td>';
-			echo'</tr>';
+    <!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+	<style>
+		body {
+			background-image: url("https://webdev.cs.uiowa.edu/~kwang9/project/image/FoodBackground.jpg");
 		}
-?>
-	
-	</table>	
-
-	
-	
-	
-	
-	
-	
-	</body>
-</html>
+	</style>
 
 
+	</head>
+	
+	<body>
+		
+		<div class="container">
+		<!--Container for all content to be displayed-->
+			<div class="row">
+				<div class="col-xs-12">
+					<div class="page-header">
+					<!--Header-->		
+						<h1><b>Hvyee.com</b></font></h1>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		
+			<div class="jumbotron">
+				<div class="row">
+					<div class="col-sm-9 col-xs-12">
+						<h1>Welcome to Hvyee !</h1>
+						<p><a class="btn btn-primary btn-lg" href="https://webdev.cs.uiowa.edu/~kwang9/project/shopperlogin.php" role="button">Login</a>&nbsp;<a class="btn btn-primary btn-lg" href="https://webdev.cs.uiowa.edu/~kwang9/project/browsePguest.php" role="button">Products</a>&nbsp;<a class="btn btn-primary btn-lg" href="https://webdev.cs.uiowa.edu/~kwang9/project/browseCguest.php" role="button">Categories</a>&nbsp;<a class="btn btn-primary btn-lg" href="https://webdev.cs.uiowa.edu/~kwang9/project/ordersguest.php" role="button">Orders</a>&nbsp;<a class="btn btn-primary btn-lg" href="https://webdev.cs.uiowa.edu/~kwang9/project/inputUser.php" role="button">Register</a>&nbsp;<a class="btn btn-primary btn-lg" href="https://webdev.cs.uiowa.edu/~kwang9/project/guestcart.php" role="button">Shopping Cart</a></p>
+					</div>	
+				</div>	
+			</div>
+		
+		
+<!-- thumbnail icons /-->
+		<div class="containter">			
+			<div class="row">
+				<div class="col-xs-6 col-md-3">
+					<a href="#" class="thumbnail">
+						<?php											/*
+						 *List all Productes that are in the DB
+						 *
+						 */    
+						// connect to the DB
+						$db = connectDB($DBHost, $DBUser, $DBPasswd, $DBName);
+						
+						// set up a query to get infor on the cars from the DB
+						$query = 'SELECT * FROM PRODUCT order by RAND() LIMIT 1';
+						
+						// run the query
+						$result = queryDB($query, $db);
+						
+						
+						while($row = nextTuple($result))
+							{
+								if ($row['IMAGE'])				
+								{$imagelocation=$row['IMAGE'];
+								$altText="product" . $row['PNAME'];
+								echo "<img src='$imagelocation' width='150' alt=$altText'>";}
+								echo "<td><a href='Description.php?ID=" . $row['ID'] . "'>" . $row['PNAME'] . "</a></td>";
+							}
+						?>
+				  </a>
+				</div>
+				
+				
+				<div class="col-xs-6 col-md-3">
+					<a href="#" class="thumbnail">
+						<?php											/*
+						 *List all Productes that are in the DB
+						 *
+						 */    
+						// connect to the DB
+						$db = connectDB($DBHost, $DBUser, $DBPasswd, $DBName);
+						
+						// set up a query to get infor on the cars from the DB
+						$query = 'SELECT * FROM PRODUCT order by RAND() LIMIT 1';
+						
+						// run the query
+						$result = queryDB($query, $db);
+						
+						
+						while($row = nextTuple($result))
+							{
+								if ($row['IMAGE'])				
+								{$imagelocation=$row['IMAGE'];
+								$altText="product" . $row['PNAME'];
+								echo "<img src='$imagelocation' width='150' alt=$altText'>";}
+								echo "<td><a href='Description.php?ID=" . $row['ID'] . "'>" . $row['PNAME'] . "</a></td>";
+							}
+						?>
+				  </a>
+				</div>
+			
+				<div class="col-xs-6 col-md-3">
+					<a href="#" class="thumbnail">
+						<?php											/*
+						 *List all Productes that are in the DB
+						 *
+						 */    
+						// connect to the DB
+						$db = connectDB($DBHost, $DBUser, $DBPasswd, $DBName);
+						
+						// set up a query to get infor on the cars from the DB
+						$query = 'SELECT * FROM PRODUCT order by RAND() LIMIT 1';
+						
+						// run the query
+						$result = queryDB($query, $db);
+						
+						
+						while($row = nextTuple($result))
+							{
+								if ($row['IMAGE'])				
+								{$imagelocation=$row['IMAGE'];
+								$altText="product" . $row['PNAME'];
+								echo "<img src='$imagelocation' width='150' alt=$altText'>";}
+								echo "<td><a href='Description.php?ID=" . $row['ID'] . "'>" . $row['PNAME'] . "</a></td>";
+							}
+						?>
+				  </a>
+				</div>
 
-
-
-
+				<div class="col-xs-6 col-md-3">
+					<a href="#" class="thumbnail">
+						<?php											/*
+						 *List all Productes that are in the DB
+						 *
+						 */    
+						// connect to the DB
+						$db = connectDB($DBHost, $DBUser, $DBPasswd, $DBName);
+						
+						// set up a query to get infor on the cars from the DB
+						$query = 'SELECT * FROM PRODUCT order by RAND() LIMIT 1';
+						
+						// run the query
+						$result = queryDB($query, $db);
+						
+						
+						while($row = nextTuple($result))
+							{
+								if ($row['IMAGE'])				
+								{$imagelocation=$row['IMAGE'];
+								$altText="product" . $row['PNAME'];
+								echo "<img src='$imagelocation' width='150' alt=$altText'>";}
+								echo "<td><a href='Description.php?ID=" . $row['ID'] . "'>" . $row['PNAME'] . "</a></td>";
+							}
+						?>
+				  </a>
+				</div>
+				
+			</div>	
+		</div>
+		
+	
 
 
 
