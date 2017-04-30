7e63e56007c3ec8dabf48d1dfc2afe6d7076b6ae
InoriReei <marlborofornemo@gmail.com>
2017-04-30 17:32:01 -0500

    Update browsePguest.php

--- browsePguest.php
+++ browsePguest.php
@@ -1,210 +1,177 @@
-<!--browse products-->
+<!--shopper home-->
+<?php
+	include_once('config.php');
+	include_once('dbutils.php');
+?>
 <html>
-
-<!-- Boostrap link -->
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
-<?php
-include_once('config.php');
-include_once('dbutils.php');
-
-$title ="Products";
-$h1 = "Products";
-$menuActive=1;
-include_once("guestheader.php");
-?>
-
-
-<!-- sumbit order -->
-
-<?php
-
-//include config and utils files
-include_once('config.php');
-include_once('dbutils.php');
-
-//check if form data needs to be processed
-if (isset($_POST['submit'])) {
-    //if we are here, the form is sumbitted and we need to process form data
+	<head>
+	<meta name="viewport" content="width=device-widthe, initial-scale-1.0">
     
-    //get the data from the form
-    $NAME = $_POST['Name'];
-    $QTY = $_POST['QTY'];
-	$productid = $_POST['PRODUCT-ID'];
-    
-    // variable to keep track if the from is complete
-    $isComplete = true;
-    
-    //error message we'll give user in case there are issues with data
-    $errorMessage = "";
-    
-    
-    if (!$Name) {
-        $errorMessage = "Please enter the product name. \n";
-        $isComplete = false;
-    }    
+    <!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-    if (!$QTY) {
-        $errorMessage = "Please enter the number of selected items. \n";
-        $isComplete = false;
-    }
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
-//stop the php and how error message    
-    if (!$isComplete) {
-        punt($errorMessage);
-    }
-    
-    //put together sql statement to insert new data
-    $query = "INSERT INTO ORDERS(PRODUCTID, QTY) VALUES('$productid', '$QTY');";
-    
-    //connect to the DB
-    $db = connectDB($DBHost, $DBUser, $DBPasswd, $DBName);
-    
-    //run the insert statement
-    $result - queryDB($query, $db);
-    
-    //we have successfully entered the date
-    echo ("Successfully Sumbitted new order: " . $Name);
-    
-    //reset so we can reset the form since we've successfully added the data
-    unset($isComplete, $errorMessage, $Name, $QTY, $Sequentialid);
-}
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
-?>
-
-    <!-- Title -->
-    <div class="row">
-        <div class="col-xs-12">
-            <h1>New Orders</h1>
-        </div>
-    </div>
+	<style>
+		body {
+			background-image: url("https://webdev.cs.uiowa.edu/~kwang9/project/image/FoodBackground.jpg");
+		}
+	</style>
 	
-
-        <div class="row">
-            <div class="col-xs-12">
-                
-                <form action="browsePguest.php" method="post">
-                         
-                <!-- Dropdown for product -->
-					<div class="form-group">
-						<label for="NAME">Product Name:</label>
-						<?php
-						//connect to DB
-						if (!isset($db)) {
-							$db = connectDB($DBHost, $DBUser, $DBPasswd, $DBName);
-						}
-						echo (generateDropdown($db, "PRODUCT", "NAME", "ID", $productid));
-						?>
-						
-					</div>
-                
-                <!-- Origin --> 
-                    <div class="form-group">
-                        <label for="QTY">QTY:</label>
-                        <input type="number" class="form-control" name="QTY"/>
-                    </div>
-                
-                <button type="submit" class="btn btn-default" name="submit">Submit</button>
-                
-                </form>
-            </div>
-        </div>
-
-
-
-
-
-
-
-
+	</head>
+	
 	<body>
-<!-- Browse Product -->
+		
+		<div class="container">
+		<!--Container for all content to be displayed-->
+			<div class="row">
+				<div class="col-xs-12">
+					<div class="page-header">
+					<!--Header-->		
+						<h1><b>Hvyee.com</b></font></h1>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		
+			<div class="jumbotron">
+				<div class="row">
+					<div class="col-sm-9 col-xs-12">
+						<h1>Welcome to Hvyee !</h1>
+						<p><a class="btn btn-primary btn-lg" href="https://webdev.cs.uiowa.edu/~kwang9/project/shopperlogin.php" role="button">Login</a>&nbsp;<a class="btn btn-primary btn-lg" href="https://webdev.cs.uiowa.edu/~kwang9/project/browsePguest.php" role="button">Products</a>&nbsp;<a class="btn btn-primary btn-lg" href="https://webdev.cs.uiowa.edu/~kwang9/project/browseCguest.php" role="button">Categories</a>&nbsp;<a class="btn btn-primary btn-lg" href="https://webdev.cs.uiowa.edu/~kwang9/project/ordersguest.php" role="button">Orders</a>&nbsp;<a class="btn btn-primary btn-lg" href="https://webdev.cs.uiowa.edu/~kwang9/project/inputUser.php" role="button">Register</a>&nbsp;<a class="btn btn-primary btn-lg" href="https://webdev.cs.uiowa.edu/~kwang9/project/guestcart.php" role="button">Shopping Cart</a></p>
+					</div>	
+				</div>	
+			</div>
+		
+		
+<!-- thumbnail icons /-->
+		<div class="containter">			
+			<div class="row">
+				<div class="col-xs-6 col-md-3">
+					<a href="#" class="thumbnail">
+						<?php											/*
+						 *List all Productes that are in the DB
+						 *
+						 */    
+						// connect to the DB
+						$db = connectDB($DBHost, $DBUser, $DBPasswd, $DBName);
+						
+						// set up a query to get infor on the cars from the DB
+						$query = 'SELECT * FROM PRODUCT order by RAND() LIMIT 1';
+						
+						// run the query
+						$result = queryDB($query, $db);
+						
+						
+						while($row = nextTuple($result))
+							{
+								if ($row['IMAGE'])				
+								{$imagelocation=$row['IMAGE'];
+								$altText="product" . $row['PNAME'];
+								echo "<img src='$imagelocation' width='150' height='150' alt=$altText'>";}
+								echo "<td><a href='Description.php?ID=" . $row['ID'] . "'>" . $row['PNAME'] . "</a></td>";
+							}
+						?>
+				  </a>
+				</div>
+				
+				
+				<div class="col-xs-6 col-md-3">
+					<a href="#" class="thumbnail">
+						<?php											/*
+						 *List all Productes that are in the DB
+						 *
+						 */    
+						// connect to the DB
+						$db = connectDB($DBHost, $DBUser, $DBPasswd, $DBName);
+						
+						// set up a query to get infor on the cars from the DB
+						$query = 'SELECT * FROM PRODUCT order by RAND() LIMIT 1';
+						
+						// run the query
+						$result = queryDB($query, $db);
+						
+						
+						while($row = nextTuple($result))
+							{
+								if ($row['IMAGE'])				
+								{$imagelocation=$row['IMAGE'];
+								$altText="product" . $row['PNAME'];
+								echo "<img src='$imagelocation' width='150' height='150' alt=$altText'>";}
+								echo "<td><a href='Description.php?ID=" . $row['ID'] . "'>" . $row['PNAME'] . "</a></td>";
+							}
+						?>
+				  </a>
+				</div>
+			
+				<div class="col-xs-6 col-md-3">
+					<a href="#" class="thumbnail">
+						<?php											/*
+						 *List all Productes that are in the DB
+						 *
+						 */    
+						// connect to the DB
+						$db = connectDB($DBHost, $DBUser, $DBPasswd, $DBName);
+						
+						// set up a query to get infor on the cars from the DB
+						$query = 'SELECT * FROM PRODUCT order by RAND() LIMIT 1';
+						
+						// run the query
+						$result = queryDB($query, $db);
+						
+						
+						while($row = nextTuple($result))
+							{
+								if ($row['IMAGE'])				
+								{$imagelocation=$row['IMAGE'];
+								$altText="product" . $row['PNAME'];
+								echo "<img src='$imagelocation' width='150' height='150' alt=$altText'>";}
+								echo "<td><a href='Description.php?ID=" . $row['ID'] . "'>" . $row['PNAME'] . "</a></td>";
+							}
+						?>
+				  </a>
+				</div>
 
-
-<div class='row'>
-    <div class="col-sm-9 col-xs-12">
-        <p>
-            <h1>Product Information</h1>
-        </p>
-    </div>
-</div>
-
-
-<!-- Content table here -->
-    <div class='row'>
-            <div class='col-xs-12'>
-<table class='table table-hover'>
-    <!-- header for the table -->
-    <thead>
-        <th>Name</th>
-        <th>Description</th>
-        <th>Category</th>
-        <th>Price</th>
-        <th>QTY</th>
-		<th> </th>
-    </thead>
-
-<?php
-    /*
-     *List all Productes that are in the DB
-     *
-     */
-    //include config and utils files
-    include_once('config.php');
-    include_once('dbutils.php');
-    
-    // connect to the DB
-    $db = connectDB($DBHost, $DBUser, $DBPasswd, $DBName);
-    
-    // set up a query to get infor on the cars from the DB
-    $query = 'SELECT PRODUCT.NAME as Name, PRODUCT.DESCRIPTION as Description, PRODUCT.CATEGORY as Category, PRODUCT.PRICE as Price, PRODUCT.QTY FROM PRODUCT;';
-    
-    // run the query
-    $result = queryDB($query, $db);
-    
-    while($row = nextTuple($result)) {
-        echo "\n <tr>";
-        echo "<td>" .$row['Name'] . "</td>";
-        echo "<td>" .$row['Description'] . "</td>";
-        echo "<td>" .$row['Category'] . "</td>";
-        echo "<td>" .$row['Price'] . "</td>";
-        echo "<td>" .$row['QTY'] . "</td>";
-		echo "<td>" .'<a href="' . 'ordersguest.php' . '">Add to Cart</a>' . "</td>";
-        echo "</tr> \n";
-    }
-?>
+				<div class="col-xs-6 col-md-3">
+					<a href="#" class="thumbnail">
+						<?php											/*
+						 *List all Productes that are in the DB
+						 *
+						 */    
+						// connect to the DB
+						$db = connectDB($DBHost, $DBUser, $DBPasswd, $DBName);
+						
+						// set up a query to get infor on the cars from the DB
+						$query = 'SELECT * FROM PRODUCT order by RAND() LIMIT 1';
+						
+						// run the query
+						$result = queryDB($query, $db);
+						
+						
+						while($row = nextTuple($result))
+							{
+								if ($row['IMAGE'])				
+								{$imagelocation=$row['IMAGE'];
+								$altText="product" . $row['PNAME'];
+								echo "<img src='$imagelocation' width='150' height='150' alt=$altText'>";}
+								echo "<td><a href='Description.php?ID=" . $row['ID'] . "'>" . $row['PNAME'] . "</a></td>";
+							}
+						?>
+				  </a>
+				</div>
+				
+			</div>	
+		</div>
+		
 	
-<?php
-	// picture
-	echo "<td>";
-	if ($row['picture']) {
-		$imageLocation = $row['picture'];
-		$altText = '' . $row['Name'];
-		echo "<img src='$imageLocation' width='150' alt='$altText'>";
-	}
-	echo "</td>";
-
-?>	
-	
-</table>	
-
-
-
-
-
 
 
 
 <?php
-	include_once('footer.php');
+	include_once("footer.php");
 ?>
-
-	</body>
-</html>
