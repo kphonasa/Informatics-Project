35b9751168173eb8d6d65e85b1a952deeded38e2
kphonasa <pwnasa@gmail.com>
2017-05-04 02:37:24 -0500

    Add files via upload

--- footer.php
+++ footer.php
@@ -5,7 +5,6 @@
 	<p>Contact information: <a href="mailto:someone@example.com">someone@example.com</a>.</p>
 </footer>
 
-<p><strong>Note:</strong> The footer tag is not supported AT ALL</p>
 
 </body>
 </html>
