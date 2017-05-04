e442344070d1e7aee0c7a0c56a96213d347fce94
InoriReei <marlborofornemo@gmail.com>
2017-05-03 20:45:12 -0500

    Update shopperhome.php

--- shopperhome.php
+++ shopperhome.php
@@ -19,49 +19,14 @@
 <?php
 	include_once('config.php');
 	include_once('dbutils.php');
+	include_once('shopperheader.php');
 ?>
-<html>
-	<head>
-	<meta name="viewport" content="width=device-widthe, initial-scale-1.0">
-    
-    <!-- Latest compiled and minified CSS -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-	<!-- Optional theme -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
-	<!-- Latest compiled and minified JavaScript -->
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
-	<style>
-		body {
-			background-image: url("https://webdev.cs.uiowa.edu/~kwang9/project/image/FoodBackground.jpg");
-		}
-	</style>
-
-
-	</head>
-	
-	<body>
-		
-		<div class="container">
-		<!--Container for all content to be displayed-->
-			<div class="row">
-				<div class="col-xs-12">
-					<div class="page-header">
-					<!--Header-->		
-						<h1><b>Hvyee.com</b></font></h1>
-					</div>
-				</div>
-			</div>
-		</div>
 		
 		
 			<div class="jumbotron">
 				<div class="row">
 					<div class="col-sm-9 col-xs-12">
 						<h1>Welcome to Hvyee !</h1>
-						<p><a class="btn btn-primary btn-lg" href="shopperhome.php" role="button">Home</a>&nbsp;<a class="btn btn-primary btn-lg" href="browseP.php" role="button">Products</a>&nbsp;<a class="btn btn-primary btn-lg" href="browseC.php" role="button">Categories</a>&nbsp;<a class="btn btn-primary btn-lg" href="orders.php" role="button">Orders</a>&nbsp;<a class="btn btn-primary btn-lg" href="profile.php" role="button">Profile</a>&nbsp;<a class="btn btn-primary btn-lg" href="cart.php" role="button">Shopping Cart</a>&nbsp;<a class="btn btn-primary btn-lg" href="shopperlogout.php" role="button">Logout</a>&nbsp;</p>
 					</div>	
 				</div>	
 			</div>
@@ -70,8 +35,6 @@
 <!-- thumbnail icons /-->
 		<div class="containter">			
 			<div class="row">
-				<div class="col-xs-6 col-md-3">
-					<a href="#">
 						<?php											/*
 						 *List all Productes that are in the DB
 						 *
@@ -80,7 +43,7 @@
 						$db = connectDB($DBHost, $DBUser, $DBPasswd, $DBName);
 						
 						// set up a query to get infor on the cars from the DB
-						$query = 'SELECT * FROM PRODUCT order by RAND() LIMIT 1';
+						$query = 'SELECT * FROM PRODUCT order by RAND() LIMIT 4';
 						
 						// run the query
 						$result = queryDB($query, $db);
@@ -91,97 +54,12 @@
 								if ($row['IMAGE'])				
 								{$imagelocation=$row['IMAGE'];
 								$altText="product" . $row['PNAME'];
-								echo "<a href='Description.php?ID=" . $row['ID'] . "'><img src='$imagelocation' width='150' height='150' alt=$altText'>";}
-								echo "<td><a href='Description.php?ID=" . $row['ID'] . "'>" . $row['PNAME'] . "</a></td>";
+								echo '<div class="col-xs-3 col-md-3">';
+								echo "<a href='Description.php?ID=" . $row['ID'] . "'><img src='$imagelocation' width='150' height='150' alt=$altText'><BR>";}
+								echo "<a href='Description.php?ID=" . $row['ID'] . "'>" . $row['PNAME'] . "</a>";
+								echo '</div>';
 							}
 						?>
-				  </a>
-				</div>
-				
-				
-				<div class="col-xs-6 col-md-3">
-					<a href="#">
-						<?php											/*
-						 *List all Productes that are in the DB
-						 *
-						 */    
-						// connect to the DB
-						$db = connectDB($DBHost, $DBUser, $DBPasswd, $DBName);
-						
-						// set up a query to get infor on the cars from the DB
-						$query = 'SELECT * FROM PRODUCT order by RAND() LIMIT 1';
-						
-						// run the query
-						$result = queryDB($query, $db);
-						
-						
-						while($row = nextTuple($result))
-							{
-								if ($row['IMAGE'])				
-								{$imagelocation=$row['IMAGE'];
-								$altText="product" . $row['PNAME'];
-								echo "<a href='Description.php?ID=" . $row['ID'] . "'><img src='$imagelocation' width='150' height='150' alt=$altText'>";}
-								echo "<td><a href='Description.php?ID=" . $row['ID'] . "'>" . $row['PNAME'] . "</a></td>";
-							}
-						?>
-				  </a>
-				</div>
-			
-				<div class="col-xs-6 col-md-3">
-					<a href="#">
-						<?php											/*
-						 *List all Productes that are in the DB
-						 *
-						 */    
-						// connect to the DB
-						$db = connectDB($DBHost, $DBUser, $DBPasswd, $DBName);
-						
-						// set up a query to get infor on the cars from the DB
-						$query = 'SELECT * FROM PRODUCT order by RAND() LIMIT 1';
-						
-						// run the query
-						$result = queryDB($query, $db);
-						
-						
-						while($row = nextTuple($result))
-							{
-								if ($row['IMAGE'])				
-								{$imagelocation=$row['IMAGE'];
-								$altText="product" . $row['PNAME'];
-								echo "<a href='Description.php?ID=" . $row['ID'] . "'><img src='$imagelocation' width='150' height='150' alt=$altText'>";}
-								echo "<td><a href='Description.php?ID=" . $row['ID'] . "'>" . $row['PNAME'] . "</a></td>";
-							}
-						?>
-				  </a>
-				</div>
-
-				<div class="col-xs-6 col-md-3">
-					<a href="#">
-						<?php											/*
-						 *List all Productes that are in the DB
-						 *
-						 */    
-						// connect to the DB
-						$db = connectDB($DBHost, $DBUser, $DBPasswd, $DBName);
-						
-						// set up a query to get infor on the cars from the DB
-						$query = 'SELECT * FROM PRODUCT order by RAND() LIMIT 1';
-						
-						// run the query
-						$result = queryDB($query, $db);
-						
-						
-						while($row = nextTuple($result))
-							{
-								if ($row['IMAGE'])				
-								{$imagelocation=$row['IMAGE'];
-								$altText="product" . $row['PNAME'];
-								echo "<a href='Description.php?ID=" . $row['ID'] . "'><img src='$imagelocation' width='150' height='150' alt=$altText'>";}
-								echo "<td><a href='Description.php?ID=" . $row['ID'] . "'>" . $row['PNAME'] . "</a></td>";
-							}
-						?>
-				  </a>
-				</div>
 				
 			</div>	
 		</div>
