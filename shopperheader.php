9bd044f801fb0492f3c0629de63972d770ae9ff6
InoriReei <marlborofornemo@gmail.com>
2017-05-03 19:34:32 -0500

    Update shopperheader.php

--- shopperheader.php
+++ shopperheader.php
@@ -13,50 +13,80 @@
 	<!-- Latest compiled and minified JavaScript -->
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 	
-</head>
-
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+	
+	<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+	
+	<link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+	
 	<style>
 		body {
 			background-image: url("https://webdev.cs.uiowa.edu/~kwang9/project/image/FoodBackground.jpg");
 		}
+		
 	</style>
 	
-
-
-
+	
+	
+</head>
 <body>
-		<!--Container for all content to be displayed-->
+	
+<?php
+	include_once('config.php');
+	include_once('dbutils.php');
+?>
+
+
+	<div class="container">
+	<!--Container for all content to be displayed-->
 		<div class="row">
 			<div class="col-xs-12">
 				<div class="page-header">
-				<!--Header-->
-				
-				<h1><font color="black" face="Constantia" font size="24"><b><?php echo $h1;?></b></font></h1> 
-			</div>
-		</div>
-        
-	<nav class="navbar navbar-defult">
-		<div class="container">	
-				<!--Menu-->
-				<div class="container fluid">
-					<ul class="nav nav-tabs">
-							<li <?php if($menuActive==0){echo 'class="active"';}?>><a href="shopperhome.php">Home</a></li>
-							<li <?php if($menuActive==1){echo 'class="active"';}?>><a href="browseP.php">Browse Products</a></li>
-							<li <?php if($menuActive==2){echo 'class="active"';}?>><a href="browseC.php">Browse Categories</a></li>
-							<li <?php if($menuActive==3){echo 'class="active"';}?>><a href="orders.php">Orders</a></li>
-							<li <?php if($menuActive==4){echo 'class="active"';}?>><a href="profile.php">Profile</a></li>
-							<li <?php if($menuActive==5){echo 'class="active"';}?>><a href="cart.php">Shopping Cart</a></li>
-							<li <?php if($menuActive==6){echo 'class="active"';}?>><a href="shopperlogout.php">Logout</a></li>
-							<form action="get.php" method="get" class="navbar-form navbar-right" role="search">
-								<div class="input-group add-on">
-								  <input class="form-control" placeholder="Search for our products" name="srch-term" id="srch-term" type="text">
-								  <div class="input-group-btn">
-									<button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>							  </div>
-								</div>
-							</form>	
-						</ul>
-					</div>
+				<!--Header-->		
+					<h1><b><?php echo $h1;?></b></font></h1>
 				</div>
 			</div>
 		</div>
-		<div class="col-xs-12" id="page">
+	</div>
+	
+	<nav class="navbar navbar-inverse">
+		<div class="containter">
+			<!--Menu-->
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<ul class="nav nav-tabs">
+					<li <?php if($menuActive==0){echo 'class="active"';}?>><a href="shopperhome.php">Home</a></li>
+					<li <?php if($menuActive==1){echo 'class="active"';}?>><a href="browseP.php">Browse Products</a></li>
+					<li <?php if($menuActive==3){echo 'class="active"';}?>><a href="orders.php">Orders</a></li>
+					<li <?php if($menuActive==4){echo 'class="active"';}?>><a href="profile.php">Profile</a></li>
+					<li <?php if($menuActive==5){echo 'class="active"';}?>><a href="cart.php">Shopping Cart</a></li>
+					<li <?php if($menuActive==6){echo 'class="active"';}?>><a href="shopperlogout.php">Logout</a></li>
+					<li class="dropdown">
+						<a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Category <span class="caret"></span></a>
+						<ul class="dropdown-menu">
+							<?php
+								include_once('config.php');
+								include_once('dbutils.php');
+								$db = connectDB($DBHost, $DBUser, $DBPasswd, $DBName);											
+								// set up a query to get infor on the cars from the DB
+								$query = 'SELECT DISTINCT CATEGORY FROM PRODUCT';											
+										// run the query
+								$result = queryDB($query, $db);											
+								while($row = nextTuple($result))
+								{
+									echo "<li><a href='https://webdev.cs.uiowa.edu/~kwang9/project/browsePguest.php?CATEGORY=" . $row['CATEGORY'] . "'>" . $row['CATEGORY'] . "</a></li>";	
+								}
+							?>
+						</ul>
+					</li>
+				
+					<form action="get.php" method="get" class="navbar-form navbar-right" role="search">
+						<div class="input-group add-on">
+						  <input class="form-control" placeholder="Search for our products" name="srch-term" id="srch-term" type="text">
+							<div class="input-group-btn">
+							<button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>							  </div>
+						</div>
+					</form>
+				</ul>
+			</div>
+		</div>
+	</nav>
