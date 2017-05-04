3b2b84989cd477d4e3a97fb7cd24560d65363ad7
InoriReei <marlborofornemo@gmail.com>
2017-05-04 06:11:24 -0500

    Update inputUser.php

--- inputUser.php
+++ inputUser.php
@@ -1,34 +1,15 @@
 <?php
 	include_once('config.php');
 	include_once('dbutils.php');
+    
+    $title ="Rigister";
+    $h1 = "Rigister";
+    $menuActive=4;
+    include_once('guestheader.php');
 ?>
-
-<html>
-	<head>
-	
-	<title>Enter Users</title>
-		<neta name="viewport" content="width=device-widthe, initial-scale-1.0">
-	
-		<!-- Latest compiled and minified CSS -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-		<!-- Optional theme -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
-		<!-- Latest compiled and minified JavaScript -->
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
-	</head>
-	
 	
 	<body>
-	
-	<div class="row">
-		<div class="col-xs-12">
-			<h1>Enter Users</h1>
-		</div>
-	</div>
-	
+		
 	<!---Processing form input--->
 	<div class="row">
 		<div class="col-xs-12">
@@ -122,7 +103,6 @@
 				$isComplete = false;
 			}
 			
-
 			if ($isComplete)
 			{
 				//check if there's a user with the same email
@@ -168,143 +148,159 @@
 	
 	<!--form for inputting data-->
 	<div class="row">
-		<div class="col-xs-12">
-			<div id="container">
+		<div class="col-md-1">
+			
+		</div>
 		
-<form action = "inputUser.php" method="post">
-	<div class ="form-group">
-		<label for="email">Email</label>
-		<input type="email" style="width: 500" class="form-control" name="email"/>
-	</div>
-	
-	<div class="form-group">
-		<label for="password">Password</label>
-		<input type="password" style="width: 500" class="form-control" name="password"/>
-	</div>
-	
-	<div class="form-group">
-		<label for="password2">Enter password again</label>
-		<input type="password" style="width: 500" class="form-control" name="password2"/>
-	</div>
-	
-	<div class="form-group">
-		<label for="fname">First Name</label>
-		<input type="text" style="width: 500" class="form-control" name="fname"/>
-	</div>
-	
-	<div class="form-group">
-		<label for="lname">Last Name</label>
-		<input type="text" style="width: 500" class="form-control" name="lname"/>
-	</div>
-	
-	<div class="form-group">
-		<label for="street">Street Address</label>
-		<input type="text" style="width: 500" class="form-control" name="street"/>
-	</div>
-	
-	<div class="form-group">
-		<label for="city">City</label>
-		<input type="text" style="width: 500" class="form-control" name="city"/>
-	</div>
-	
-	<div class="form-group" > <!-- State Button -->
-		<label for="state" class="control-label">State</label>
-		<select class="form-control" style="width: 500" name="state">
-			<option value="AL">Alabama</option>
-			<option value="AK">Alaska</option>
-			<option value="AZ">Arizona</option>
-			<option value="AR">Arkansas</option>
-			<option value="CA">California</option>
-			<option value="CO">Colorado</option>
-			<option value="CT">Connecticut</option>
-			<option value="DE">Delaware</option>
-			<option value="DC">District Of Columbia</option>
-			<option value="FL">Florida</option>
-			<option value="GA">Georgia</option>
-			<option value="HI">Hawaii</option>
-			<option value="ID">Idaho</option>
-			<option value="IL">Illinois</option>
-			<option value="IN">Indiana</option>
-			<option value="IA">Iowa</option>
-			<option value="KS">Kansas</option>
-			<option value="KY">Kentucky</option>
-			<option value="LA">Louisiana</option>
-			<option value="ME">Maine</option>
-			<option value="MD">Maryland</option>
-			<option value="MA">Massachusetts</option>
-			<option value="MI">Michigan</option>
-			<option value="MN">Minnesota</option>
-			<option value="MS">Mississippi</option>
-			<option value="MO">Missouri</option>
-			<option value="MT">Montana</option>
-			<option value="NE">Nebraska</option>
-			<option value="NV">Nevada</option>
-			<option value="NH">New Hampshire</option>
-			<option value="NJ">New Jersey</option>
-			<option value="NM">New Mexico</option>
-			<option value="NY">New York</option>
-			<option value="NC">North Carolina</option>
-			<option value="ND">North Dakota</option>
-			<option value="OH">Ohio</option>
-			<option value="OK">Oklahoma</option>
-			<option value="OR">Oregon</option>
-			<option value="PA">Pennsylvania</option>
-			<option value="RI">Rhode Island</option>
-			<option value="SC">South Carolina</option>
-			<option value="SD">South Dakota</option>
-			<option value="TN">Tennessee</option>
-			<option value="TX">Texas</option>
-			<option value="UT">Utah</option>
-			<option value="VT">Vermont</option>
-			<option value="VA">Virginia</option>
-			<option value="WA">Washington</option>
-			<option value="WV">West Virginia</option>
-			<option value="WI">Wisconsin</option>
-			<option value="WY">Wyoming</option>
-		</select>	
-			</div>
-	</div>
-	
-	<div class="form-group">
-		<label for="zip">Zip Code</label>
-		<input type="text" style="width: 500" class="form-control" name="zip"/>
-	</div>
-	
-	<div class="form-group">
-		<label for="phone">Phone Number</label>
-		<input type="text" style="width: 500" class="form-control" name="phone"/>
-	</div>
-	
-	<div class="form-group">
-		<label for="cardname">Enter the Full Name on Card</label>
-		<input type="text" style="width: 500" class="form-control" name="cardname"/>
-	</div>
-	
-	<div class="form-group">
-		<label for="cardnumber">Card Number</label>
-		<input type="text" style="width: 500" class="form-control" name="cardnumber"/>
-	</div>
-	
-	<div class="form-group">
-		<label for="exmonth">Expiration Month</label>
-		<input type="text" style="width: 100" class="form-control" name="exmonth"/>
-	</div>
-
-	<div class="form-group">
-		<label for="exyear">Expiration Year</label>
-		<input type="text" style="width: 100" class="form-control" name="exyear"/>
-	</div>
-	
-	<div class="form-group">
-		<label for="ccv">CCV</label>
-		<input type="text" style="width: 100" class="form-control" name="ccv"/>
-	</div>
-	
-	<button type ="submit" class="btn btn-default" name="submit">Add</button>
-</form>
+		<div class="col-md-11 col-xs-12">
+		
+			<form action = "inputUser.php" method="post">
+				
+				<div class ="form-group col-md-4">
+					<label for="email">Email</label>
+					<input type="email" class="form-control" placeholder="Email Address" name="email"/>
+				</div>
+				
+				<div class="form-group col-md-4">
+					<label for="password">Password</label>
+					<input type="password" class="form-control" placeholder="Password" name="password"/>
+				</div>
+				
+				<div class="form-group col-md-4">
+					<label for="password2">Enter password again</label>
+					<input type="password" class="form-control" placeholder="Confirm Password" name="password2"/>
+				</div>
+				
+				<div class="form-group col-xs-6">
+					<label for="fname">First Name</label>
+					<input type="text" class="form-control" placeholder="FirstName" name="fname"/>
+				</div>
+				
+				<div class="form-group col-md-6">
+					<label for="lname">Last Name</label>
+					<input type="text" class="form-control" placeholder="LastName" name="lname"/>
+				</div>
+				
+				<div class="form-group col-md-6">
+					<label for="street">Street Address</label>
+					<input type="text" class="form-control" placeholder="Street Adress" name="street"/>
+				</div>
+				
+				<div class="form-group col-md-6">
+					<label for="city">City</label>
+					<input type="text" class="form-control" placeholder="City Name" name="city"/>
+				</div>
+				
+				<div class="form-group col-md-4"> <!-- State Button -->
+					<label for="state" class="control-label">State</label>
+					<select class="form-control" name="state">
+						<option value="AL">Alabama</option>
+						<option value="AK">Alaska</option>
+						<option value="AZ">Arizona</option>
+						<option value="AR">Arkansas</option>
+						<option value="CA">California</option>
+						<option value="CO">Colorado</option>
+						<option value="CT">Connecticut</option>
+						<option value="DE">Delaware</option>
+						<option value="DC">District Of Columbia</option>
+						<option value="FL">Florida</option>
+						<option value="GA">Georgia</option>
+						<option value="HI">Hawaii</option>
+						<option value="ID">Idaho</option>
+						<option value="IL">Illinois</option>
+						<option value="IN">Indiana</option>
+						<option value="IA">Iowa</option>
+						<option value="KS">Kansas</option>
+						<option value="KY">Kentucky</option>
+						<option value="LA">Louisiana</option>
+						<option value="ME">Maine</option>
+						<option value="MD">Maryland</option>
+						<option value="MA">Massachusetts</option>
+						<option value="MI">Michigan</option>
+						<option value="MN">Minnesota</option>
+						<option value="MS">Mississippi</option>
+						<option value="MO">Missouri</option>
+						<option value="MT">Montana</option>
+						<option value="NE">Nebraska</option>
+						<option value="NV">Nevada</option>
+						<option value="NH">New Hampshire</option>
+						<option value="NJ">New Jersey</option>
+						<option value="NM">New Mexico</option>
+						<option value="NY">New York</option>
+						<option value="NC">North Carolina</option>
+						<option value="ND">North Dakota</option>
+						<option value="OH">Ohio</option>
+						<option value="OK">Oklahoma</option>
+						<option value="OR">Oregon</option>
+						<option value="PA">Pennsylvania</option>
+						<option value="RI">Rhode Island</option>
+						<option value="SC">South Carolina</option>
+						<option value="SD">South Dakota</option>
+						<option value="TN">Tennessee</option>
+						<option value="TX">Texas</option>
+						<option value="UT">Utah</option>
+						<option value="VT">Vermont</option>
+						<option value="VA">Virginia</option>
+						<option value="WA">Washington</option>
+						<option value="WV">West Virginia</option>
+						<option value="WI">Wisconsin</option>
+						<option value="WY">Wyoming</option>
+					</select>					
+				</div>
+				
+				<div class="form-group col-md-6">
+					<label for="zip">Zip Code</label>
+					<input type="text" class="form-control" placeholder="Zip Code" name="zip"/>
+				</div>
+				
+				<div class="form-group col-md-6">
+					<label for="phone">Phone Number</label>
+					<input type="text" class="form-control" placeholder="Phone Number" name="phone"/>
+				</div>
+				
+				<div class="form-group col-md-4">
+					<label for="cardname">Enter the Full Name on Card</label>
+					<input type="text" class="form-control" placeholder="Name on Card" name="cardname"/>
+				</div>
+				
+				<div class="form-group col-md-6">
+					<label for="cardnumber">Card Number</label>
+					<input type="text" class="form-control" placeholder="Card Number" name="cardnumber"/>
+				</div>
+				
+				<div class="form-group col-md-4">
+					<label for="exmonth">Expiration Month</label>
+					<input type="text" class="form-control" placeholder="Expiration Month" name="exmonth"/>
+				</div>
+			
+				<div class="form-group col-md-4">
+					<label for="exyear">Expiration Year</label>
+					<input type="text" class="form-control" placeholder="Expiration Year" name="exyear"/>
+				</div>
+				
+				<div class="form-group col-md-3">
+					<label for="ccv">CCV</label>
+					<input type="text" class="form-control" placeholder="CCV" name="ccv"/>
+				</div>
+				
+				<div class="containter">
+					<div class="row">
+						<div class="col-md-11">
+							<button type ="submit" class="btn btn-default" name="submit">Add</button>
+						</div>
+					</div>
+				</div>
+			</form>
+		
 		</div>
 	</div>
-	
-	<p>Already have an account? Click <a href="shopperlogin.php"> here</a> to login.</p>
-	</body>
-</html>
+		
+				<div class="col-md-11">
+					<p>Already have an account? Click <a href="shopperlogin.php"> here</a> to login.</p>
+				</div>
+				
+				
+			
+<?php
+	include_once('footer.php');
+?>
