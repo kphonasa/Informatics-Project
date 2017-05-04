f0fbd1e845f5c67308037a0dc14217eba956a343
InoriReei <marlborofornemo@gmail.com>
2017-05-04 00:51:26 -0500

    Update selectSguest.php

--- selectSguest.php
+++ selectSguest.php
@@ -1,16 +1,22 @@
-
 <?php
 	include_once('config.php');
 	include_once('dbutils.php');
 ?>
-
+<?php
+// this kicks users out if they are not logged in
+    session_start();
+    if (!isset($_SESSION['COOKIE'])) {
+        header('Location: guesthome.php');
+        exit;
+    }
+?>
 	
 <div class="container">
-<form action="selectSguest.php" method="post">
+<form action="selectS.php" method="post">
 <!--maker-->
 <div class="form-group">
     <label for="STORE">Select Store:</label>
-        <select class="form-control" style="width: 500" name="STORE">
+        <select class="form-control" style=width: "500" name="STORE">
 		<?php 
 		$db = connectDB($DBHost,$DBUser,$DBPasswd,$DBName);
 		//run the query
