c7efd674fb84608d3feb72106f55733d6cfdf2fb
Sam Martinez <Jrakkez@users.noreply.github.com>
2017-10-31 19:55:02 -0700

    Got rid of unnecessary code

--- Guest-Pages/browseC2guest.php
+++ Guest-Pages/browseC2guest.php
@@ -1,6 +1,5 @@
 <!--browse categories-->
 <?php
-//kicks users out if they are not logged in
         session_start();
 	
 		if (!isset($_SESSION['STORE']))
@@ -24,8 +23,6 @@ $CID=$_GET['ID'];
 		<div class="col-xs-12">
 			<div id="container">
 			<form action = "browseC2guest.php?ID=<?php echo ($CID); ?>" method="post">
-			<!--<input  type="text" name="name"> 
-	<input  type="submit" class="btn btn-default" name="search" value="Search"> -->
 	<select class="form-control" style="width: 200" name="order" data-default-value=<?php $query ?>>
 		<option selected disabled hidden>Order By:</option>
 		<option value="SELECT PRODUCT.ID, PRODUCT.PNAME, PRODUCT.CATEGORYID, PRODUCT.IMAGE, PRODUCT.PRICE, CATEGORY.CNAME FROM PRODUCT, CATEGORY WHERE CATEGORY.ID=PRODUCT.CATEGORYID AND PRODUCT.STOREID=<?php echo($_SESSION['STORE']); ?> AND PRODUCT.CATEGORYID=<?php echo ($CID); ?> ORDER BY PRODUCT.PNAME ASC;">A-Z</option>
@@ -44,8 +41,6 @@ $CID=$_GET['ID'];
 			<th>Product</th>
 			<th>Category</th>
 			<th>Price</th>
-			<!--<th>Quantity</th>
-			<th>Add to Cart</th>-->
 		</thead>
 		<!--include config and util files-->
 		<?php
@@ -54,8 +49,6 @@ $CID=$_GET['ID'];
 		//run the query
 		if (isset($_POST['order']))
 		{$query = $_POST['order'];}
-		//else if (isset($_POST['search']))
-		//{$query ="SELECT PRODUCT.ID, PRODUCT.PNAME, PRODUCT.CATEGORYID, PRODUCT.IMAGE, PRODUCT.PRICE, CATEGORY.CNAME FROM PRODUCT, CATEGORY WHERE PRODUCT.STOREID='" . $_SESSION['STORE'] . "' AND PRODUCT.CATEGORYID='" . $CID . "' AND PRODUCT.PNAME LIKE '%" . $_POST['name'] . "%';";}
 		else{$query ="SELECT PRODUCT.ID, PRODUCT.PNAME, PRODUCT.CATEGORYID, PRODUCT.IMAGE, PRODUCT.PRICE, CATEGORY.CNAME FROM PRODUCT, CATEGORY WHERE CATEGORY.ID=PRODUCT.CATEGORYID AND PRODUCT.STOREID='" . $_SESSION['STORE'] . "' AND CATEGORY.ID='" . $CID . "' ORDER BY PRODUCT.PNAME ASC;";}
 	
 		$result= queryDB($query, $db);
@@ -73,8 +66,6 @@ $CID=$_GET['ID'];
 			echo "<td><a href='Descriptionguest.php?ID=" . $row['ID'] . "CID=" . $CID . "'>" . $row['PNAME'] . "</a></td>";
 			echo '<td>' . $row['CATEGORY'] . '</td>';
 			echo '<td>'; echo"$"; echo $row['PRICE']; echo'</td>';
-			//echo '<td>'; echo"Quantity"; echo"<form method='post' action='browseC2.php?ID=" . $row['ID'] . "'><input type='text' name='quantity' size='2'/>"; echo '</td>';
-			//echo '<td>'; echo"<button type ='submit' class='btn btn-default' name='Add'>Add to Cart</button></form>";echo'</td>';
 			if (isset($_POST['Add']))
 			{$QTY=$_POST['quantity'];
 			
@@ -94,4 +85,3 @@ $CID=$_GET['ID'];
 <?php
 	include_once("footer.php");
 ?>
-Contact GitHub API Training Shop Blog About
